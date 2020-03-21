ebed294ddfb946f6f29f1f84084df637118c62fa
DEBJIT <karmadebjit@gmail.com>
2020-03-21 11:04:16 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function generateChoice()
 var randomNo=0;
 var lastrandom=0;
 while (randomNo === lastrandom) {
-    random = Math.floor(Math.random() * 3) + 1;
+    randomNo = Math.floor(Math.random() * 3) + 1;
 }
 lastrandom = randomNo;
 if(randomNo==1)
@@ -37,16 +37,16 @@ function results()
 {
 if(computerChoose==userChoose)
 	return result="tie";
-if(computerChoose=="rock" && userChoose=="paper")
+if(computerChoose=="Rock" && userChoose=="Paper")
 	return result="User wins";
-if(computerChoose=="rock" && userChoose=="scissors")
+if(computerChoose=="Rock" && userChoose=="Scissors")
 	return result="Computer wins";
-if(computerChoose=="paper" && userChoose=="scissors")
+if(computerChoose=="Paper" && userChoose=="Scissors")
 	return result="User wins";
-if(computerChoose=="scissors" && userChoose=="paper")
+if(computerChoose=="Scissors" && userChoose=="Paper")
 	return result="Computer wins";
-if(computerChoose=="scissors" && userChoose=="rock")
+if(computerChoose=="Scissors" && userChoose=="Rock")
 	return result="User wins";
-if(computerChoose=="paper" && userChoose=="rock")
+if(computerChoose=="Paper" && userChoose=="Rock")
 	return result="Computer wins";
 }
